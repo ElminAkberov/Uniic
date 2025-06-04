9134417c947094afe4098f36f86620d74f0b0c5f
ElminAkberov <ekberovelmin04@gmail.com>
2025-06-04 18:59:21 +0400

    smooth-scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Layout from "./layouts/Layout";
+import { useLenis } from "./utils/useLenis";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+  const location = useLocation();
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
+  useLenis();
   return <Layout />;
 };
 
---
+++ src/utils/useLenis.js
@@ -0,0 +1,21 @@
+import Lenis from "@studio-freight/lenis";
+import { useEffect } from "react";
+
+export const useLenis = () => {
+  useEffect(() => {
+    const lenis = new Lenis({
+      smooth: true,
+    });
+
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+};
